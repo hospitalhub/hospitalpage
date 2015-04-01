a82ab9dae8661ad16c7d1541099d33a775d4a5b4
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-01 11:51:09 +0200

    travis config

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
  * @var bool
  */
 define('WP_USE_THEMES', true);
-
+echo 'hello world! ' . phpversion() . PHP_EOL;
 /** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+// require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
