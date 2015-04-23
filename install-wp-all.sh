c0dd836a54823e73d216607d90748a4c0659fd25
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-23 10:22:47 +0200

    db

--- install-wp-all.sh
+++ install-wp-all.sh
@@ -1,4 +1,7 @@
 WP=/var/www
+wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=x
+wp blog create --url=127.0.0.1
+wp core multisite-convert
 wp plugin activate --all
 wp cap add administrator raport_zakazne
 wp rewrite structure '%postname%'
