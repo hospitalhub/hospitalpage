964f722090fe289bdcb93b456912e2cf40c5004b
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 00:20:44 +0200

    db

--- install-db.sh
+++ install-db.sh
@@ -1,13 +1,11 @@
 WP=/var/www
-cd $WP
-cp resources/.env ~/.env
-cd vendor/amarcinkowski/hospitalplugin/
+sudo cp resources/.env /root/.env
+sudo -u vagrant cp resources/.env /home/vagrant/.env
+cd $WP/vendor/amarcinkowski/hospitalplugin/
 ../../bin/doctrine orm:schema-tool:create
-cd $WP
-cd wp-content/plugins/punction/
+cd $WP/wp-content/plugins/punction/
 ../../../vendor/bin/doctrine orm:schema-tool:create
-cd $WP
-cd wp-content/plugins/punction/
+cd $WP/wp-content/plugins/punction/
 ../../../vendor/bin/doctrine orm:schema-tool:create
-echo "insert into hospital_user values (1,'1','admin');" | wp db cli
-echo "INSERT INTO hospital_ward VALUES (1, 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', 0, '', '', '', '');" | wp db cli
+echo "insert into hospital_user values (1,'1','admin');" | sudo -u vagrant wp db cli
+echo "INSERT INTO hospital_ward VALUES (1, 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', 0, '', '', '', '');" | sudo -u vagrant wp db cli
