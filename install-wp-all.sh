18c8488f47fed9a61f26870ac871948ea3a6faff
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-23 10:24:03 +0200

    i

--- install-wp-all.sh
+++ install-wp-all.sh
@@ -1,16 +1,16 @@
 WP=/var/www
+wp db create
 wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=x
-wp blog create --url=127.0.0.1
-wp core multisite-convert
-wp plugin activate --all
-wp cap add administrator raport_zakazne
-wp rewrite structure '%postname%'
+#wp blog create --url=127.0.0.1
+#wp plugin activate --all
+#wp cap add administrator raport_zakazne
+#wp rewrite structure '%postname%'
 #wp core multisite-convert
-cd vendor/amarcinkowski/hospitalplugin/
-../../bin/doctrine orm:schema-tool:create
-cd $WP
-cd wp-content/plugins/punction/
-../../../vendor/bin/doctrine orm:schema-tool:create
-cd $WP
-cd wp-content/plugins/punction/
-../../../vendor/bin/doctrine orm:schema-tool:create
+#cd vendor/amarcinkowski/hospitalplugin/
+#../../bin/doctrine orm:schema-tool:create
+#cd $WP
+#cd wp-content/plugins/punction/
+#../../../vendor/bin/doctrine orm:schema-tool:create
+#cd $WP
+#cd wp-content/plugins/punction/
+#../../../vendor/bin/doctrine orm:schema-tool:create
