0d985bf4ab7c929cccf69c481d1a27e132cf0a02
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-23 20:31:33 +0200

    db

--- install-db.sh
+++ install-db.sh
@@ -10,4 +10,4 @@ cd $WP
 cd wp-content/plugins/punction/
 ../../../vendor/bin/doctrine orm:schema-tool:create
 echo "insert into hospital_user values (1,'1','admin');" | wp db cli
-echo "INSERT INTO `hospital`.`hospital_ward` (`id`, `name`, `infomedica`, `komorkaOrg`, `kierownik`, `typOddzialu`, `pododdzial`, `adres`, `kondygnacja`, `odcinek`, `opis`) VALUES ('1', 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', '0', '', '', '', '');" | wp db cli
+echo "INSERT INTO hospital_ward VALUES (1, 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', 0, '', '', '', '');" | wp db cli
