5f2115ff3b74cca024b4392ad2fbeec42c9a232d
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-23 16:23:24 +0200

    cache

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,4 +4,4 @@ sudo cp /vagrant/auth.json /home/vagrant/.composer/
 sudo chown -R vagrant: /home/vagrant/.composer
 cd /var/www
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-composer up --prefer-dist --no-interaction -vv -cache-dir=/dev/null
+composer up -o --prefer-dist --no-interaction -vv --cache-dir=/dev/null
