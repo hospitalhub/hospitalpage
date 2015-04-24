a570ca5a0d89cf5633f0287283b4b8a6992fae19
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 08:05:37 +0200

    fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-composer config -g github-oauth.github.com $1
 COMPOSER_CACHE_DIR=/dev/null
 mkdir /home/vagrant/.composer
 sudo cp /vagrant/auth.json /home/vagrant/.composer/
 sudo chown -R vagrant: /home/vagrant/.composer
 cd /var/www
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+composer config -g github-oauth.github.com $1
 composer up -o --prefer-dist --no-interaction -vv --no-dev
