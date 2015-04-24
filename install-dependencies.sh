0db0b8cfd3a1321b2ee08e4e4f6822e3c77ea739
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 09:18:04 +0200

    deps

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 COMPOSER_CACHE_DIR=/dev/null
+echo "composer@$USER"
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+composer config -g github-oauth.github.com $1
 cd /var/www
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-sudo -u vagrant composer config -g github-oauth.github.com $1
-sudo -u vagrant composer up -o --prefer-dist --no-interaction -vv --no-dev
+composer install -o --prefer-dist --no-interaction -vv --no-dev
