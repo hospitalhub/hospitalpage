5a194b984984d910fb0905fae9de287cb383e40e
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-27 11:31:39 +0200

    multisite

--- install-wp-all.sh
+++ install-wp-all.sh
@@ -2,13 +2,13 @@
 cd /var/www
 cp resources/.env .
 wp db create
-wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=x
+wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=pass
 wp plugin activate --all
 wp cap add administrator raport_zakazne
 wp cap add administrator raporty_zakazne_zbiorczy
 wp cap add administrator edit_pacjents
 wp cap add administrator read_pacjenci_wszystkie_oddz
-wp rewrite structure '%postname%'
-wp core multisite-convert
+#wp rewrite structure '%postname%'
+#wp core multisite-convert
 wp blog create --url=127.0.0.1
 wp rewrite structure '%postname%'
