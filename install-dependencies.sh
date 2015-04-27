ba77fca5421cea4138fe02f29475fce5c49a9012
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-27 12:20:08 +0200

    deps

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-COMPOSER_CACHE_DIR=/dev/null
 echo "composer@$USER"
 sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+echo "adding github keys"
 ssh-keyscan github.com >> ~/.ssh/known_hosts
+echo "composer config install"
 composer config -g github-oauth.github.com $1
 cd /var/www
 composer install -o --prefer-dist --no-interaction -vv
