943217dca3da30b30874cc6a4ea4b67a23426893
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-06-16 16:13:02 +0200

    multisite install

--- scripts/install-wp-all.sh
+++ scripts/install-wp-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www
-export MULTISITE=false
 wp db create
-wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=pass
+wp core multisite-install --url=127.0.0.1 --base=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=pass
+wp rewrite structure '%postname%'
 wp core language activate pl_PL
 wp plugin activate --all
 wp theme activate responsive-child
@@ -10,8 +10,6 @@ wp cap add administrator raport_zakazne
 wp cap add administrator raporty_zakazne_zbiorczy
 wp cap add administrator edit_pacjents
 wp cap add administrator read_pacjenci_wszystkie_oddz
-wp rewrite structure '%postname%'
-export MULTISITE=true
 #wp core multisite-convert
-wp blog create --url=127.0.0.1
-wp rewrite structure '%postname%'
+#wp blog create --url=127.0.0.1
+#wp rewrite structure '%postname%'
