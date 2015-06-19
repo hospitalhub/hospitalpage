a9e85225d004e67e3b2ea4f282664637d49d1ab0
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-06-19 16:14:37 +0200

    fastcgi @travis

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -71,12 +71,17 @@ deb http://security.ubuntu.com/ubuntu trusty-security multiverse" >> /etc/apt/so
   echo "127.0.0.1 mydomain.local" | tee --append /etc/hosts
 else
   echo "@travis";
-  sudo apt-get update
-  sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-  echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
   export DB_USER=root
   export DB_PASSWORD=
+  sudo apt-get update
+  sudo apt-get install apache2 libapache2-mod-fastcgi
+  # enable php-fpm
+  sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
   sudo a2enmod rewrite actions fastcgi alias
+  echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+  # configure apache virtual hosts
+  sudo cp -f resources/travis-ci-apache /etc/apache2/sites-available/default
+  sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
   sudo service apache2 restart
 fi
