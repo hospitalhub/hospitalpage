22938504679663c329c04b2ab3e1f9138a7f307c
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-06-19 16:35:24 +0200

    vagrant / travis provisioning

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,8 +6,15 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 echo "composer config install"
 composer config -g github-oauth.github.com $1
 if [ "$USER" = "vagrant" ]; then
+  echo "@VAGRANT dependencies"
   composer config --global cache-dir /vagrant/cache/composer
   cp resources/.env /home/vagrant/.env
+  if [ ! -d /var/www ]; then
+    mkdir -p /var/www;
+  fi;
+  cd /var/www
+  cp resources/.env /var/www/.env
+else
+  echo "@TRAVIS dependencies dir: $PWD $(pwd)"
 fi
-cp ./resources/.env ~/.env
 composer install -o --prefer-dist --no-interaction -v
