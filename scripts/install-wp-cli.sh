b65316aacaa961b6e4befbf633294654521b05d1
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-07-13 19:13:41 +0200

    wp cli config file

--- scripts/install-wp-cli.sh
+++ scripts/install-wp-cli.sh
@@ -2,5 +2,8 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+mkdir ~/.wp-cli
+echo "path: /var/www/wp
+url: 127.0.0.1:8000" > ~/.wp-cli/config.yml
 #tests scaffolding
 #wp --path="wp/" scaffold plugin-tests punction
