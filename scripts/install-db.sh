da2c76209534ea6db7959a78b4da7c894f2c58ea
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-07-23 19:27:54 +0200

    no more entities in epidemio

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -4,7 +4,5 @@ cd $DIR/vendor/amarcinkowski/hospitalplugin/
 ../../bin/doctrine orm:schema-tool:create
 cd $DIR/wp-content/plugins/punction/
 ../../../vendor/bin/doctrine orm:schema-tool:create
-cd $DIR/wp-content/plugins/epidemio
-../../../vendor/bin/doctrine orm:schema-tool:create
 echo "insert into hospital_user values (1,'1','admin');" | sudo -u vagrant wp db cli
 echo "INSERT INTO hospital_ward VALUES (1, 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', 0, '', '', '', '');" | sudo -u vagrant wp db cli
