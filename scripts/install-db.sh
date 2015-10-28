b58b480f5e25f9f519989087094fbf86adda04b6
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-10-28 11:13:23 +0100

    no orm in punction

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -3,7 +3,8 @@ DIR=/var/www
 cd $DIR/vendor/amarcinkowski/hospitalplugin/
 chmod +x /var/www/vendor/doctrine/orm/bin/doctrine
 ../../bin/doctrine orm:schema-tool:create
-cd $DIR/wp-content/plugins/punction/
-../../../vendor/bin/doctrine orm:schema-tool:create
+#moved to hospitalplugin
+#cd $DIR/wp-content/plugins/punction/
+#../../../vendor/bin/doctrine orm:schema-tool:create
 echo "insert into hospital_user values (1,'1','admin');" | sudo -u vagrant wp db cli
 echo "INSERT INTO hospital_ward VALUES (1, 'Oddział Specjalny', 'OS', 'OS', 'Ataman Młoda Foka', 'ZZ', 0, '', '', '', '');" | sudo -u vagrant wp db cli
