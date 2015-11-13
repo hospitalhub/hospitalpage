8ead3c67238633e120a2c8108695a0c8df3dcb3a
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-13 11:20:41 +0100

    travis fix

--- scripts/install-wp-cli.sh
+++ scripts/install-wp-cli.sh
@@ -3,7 +3,15 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 mkdir ~/.wp-cli
+
+if [ -z "$TRAVIS_PHP_VERSION" ]; then
+  export WP_ADDR='127.0.0.1:8000';
+  export PLATFORM='vagrant (non-travis)'
+else
+  export WP_ADDR='127.0.0.1';
+  export PLATFORM='travis-ci'
+fi
 echo "path: /var/www/wp
-url: 127.0.0.1:8000" > ~/.wp-cli/config.yml
+url: $WP_ADDR" > ~/.wp-cli/config.yml
 #tests scaffolding
 #wp --path="wp/" scaffold plugin-tests punction
