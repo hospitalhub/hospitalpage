cc5e7525dfb68c1755bb8b7855d9148f93b096b4
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-28 22:31:57 +0100

    book

---
+++ scripts/install-book.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function files_edit {
+# ADDRESSBOOK settings
+	cat resources/cfg.db.php > book/config/cfg.db.php
+	sed -i -e "s/?>/\$iplist['*.*.*.*']['role']  = "readonly";\n?>/g" -e "s/\$userlist/\/\//g" book/config/cfg.user.php
+	sed -i -e 's/?>/$disp_cols = array( "all_phones");\n?>/g' book/config/config.php
+	sed -i -e "s/<br \/>//g" -e "s/<hr \/>//g" -e "s/style=/x=/g" -e "s/button\"/\" style=\"visibility: hidden; display: none;\"/g"   book/index.php
+	echo "" > book/include/footer.inc.php > book/include/header.inc.php > book/include/format.inc.php 
+}
