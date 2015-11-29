ebfc3f5028b886972b61395412f6273c81ad2d20
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-29 15:24:08 +0100

    book

--- scripts/install-book.sh
+++ scripts/install-book.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-function link_book {
-	cd /var/www
-	if [ ! -L 'book' ]; then ln -s vendor/addressbook/addressbook book;fi
-}
 
 function files_edit {
 # ADDRESSBOOK settings
+	cd /var/www
+	if [ ! -L 'book' ]; then ln -s vendor/addressbook/addressbook book;fi
 	cat resources/cfg.db.php > book/config/cfg.db.php
 	sed -i -e "s/?>/\$iplist['*.*.*.*']['role']  = "readonly";\n?>/g" -e "s/\$userlist/\/\//g" book/config/cfg.user.php
 	sed -i -e 's/?>/$disp_cols = array("last_first", "all_phones");\n?>/g' book/config/config.php
@@ -14,5 +12,4 @@ function files_edit {
 	echo "" > book/include/footer.inc.php > book/include/header.inc.php > book/include/format.inc.php 
 }
 
-link_book
 files_edit
