0fdac6e1073cbc6ae041fb13b9ebca566c0d013d
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-12-09 13:08:37 +0100

    fix

--- scripts/selenium.sh
+++ scripts/selenium.sh
@@ -15,6 +15,6 @@ sh -e /etc/init.d/xvfb start
 echo "xvfb ready"
 sleep 2 
 mkdir -p build/logs
-java -jar vendor/netwing/selenium-server-standalone/selenium-server-standalone-2.43.0.jar >build/logs/selenium.log 2>&1
+java -jar vendor/netwing/selenium-server-standalone/selenium-server-standalone-2.43.0.jar > build/logs/selenium.log #2>&1
 echo "selenium running"
 sleep 2
